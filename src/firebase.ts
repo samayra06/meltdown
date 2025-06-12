c70bfd98edc951f9623cb54fde611bdf8f0703da
samayra <samayrajain06@gmail.com>
2025-06-12 19:56:08 +0530

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,4 @@
 // src/firebase.ts
-
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
@@ -9,8 +8,9 @@ const firebaseConfig = {
   projectId: "meltdown-787cd",
   storageBucket: "meltdown-787cd.appspot.com",
   messagingSenderId: "721027549148",
-  appId: "1:721027549148:web:b07b2fc38d2bc4e2087119",
+  appId: "1:721027549148:web:b07b2fc38d2bc4e2087119"
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+
