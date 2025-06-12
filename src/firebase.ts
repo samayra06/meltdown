78b87adcc2417c7798da4ff6c5d6743c3f37bb52
samayra <samayrajain06@gmail.com>
2025-06-12 18:53:35 +0530

    firebase.ts

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBldhvu1lPn2e3rp_YeMokN_VuBNl9Q3qA",
+  authDomain: "meltdown-787cd.firebaseapp.com",
+  projectId: "meltdown-787cd",
+  storageBucket: "meltdown-787cd.appspot.com",
+  messagingSenderId: "721027549148",
+  appId: "1:721027549148:web:b07b2fc38d2bc4e2087119",
+  measurementId: "G-7VFV9S2B23"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
