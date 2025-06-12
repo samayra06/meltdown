c62f89633a858ea4909b6f6d0a957aa2cc8eb105
samayra <samayrajain06@gmail.com>
2025-06-12 19:42:49 +0530

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,7 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+// src/firebase.ts
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBldhvu1lPn2e3rp_YeMokN_VuBNl9Q3qA",
@@ -8,7 +10,6 @@ const firebaseConfig = {
   storageBucket: "meltdown-787cd.appspot.com",
   messagingSenderId: "721027549148",
   appId: "1:721027549148:web:b07b2fc38d2bc4e2087119",
-  measurementId: "G-7VFV9S2B23"
 };
 
 const app = initializeApp(firebaseConfig);
